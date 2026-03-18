2f6d9cff73feb1d156238010eea9ed08cd9f318c
v-i-j-ay <kvijayramareddy@gmail.com>
2026-03-18 21:56:51 +0530

    .

--- config/mail.js
+++ config/mail.js
@@ -1,11 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  family: 4 // 🔥 THIS LINE FIXES YOUR ISSUE
 });
 
 const sendEmail = async (to, subject, html) => {
