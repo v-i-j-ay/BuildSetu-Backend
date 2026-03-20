afb4b2da0680745c35eef8826be77ebfe63cfb12
v-i-j-ay <kvijayramareddy@gmail.com>
2026-03-20 15:48:01 +0530

    .

--- config/mail.js
+++ config/mail.js
@@ -4,14 +4,17 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
   try {
-    await resend.emails.send({
+    const response = await resend.emails.send({
       from: "BuildSetu <onboarding@resend.dev>",
-      to: to,
-      subject: subject,
-      html: html,
+      to: [to],   
+      subject,
+      html,
     });
+
+    console.log("RESEND SUCCESS:", response);
+
   } catch (error) {
-    console.error("Email error:", error);
+    console.error("RESEND ERROR:", error);
   }
 };
 
