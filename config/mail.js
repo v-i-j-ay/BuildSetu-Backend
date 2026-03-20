33556e4548484fc54cd94c6e504b8a132dec2f0d
v-i-j-ay <kvijayramareddy@gmail.com>
2026-03-20 16:03:31 +0530

    mail verification

--- config/mail.js
+++ config/mail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, html) => {
   try {
     const response = await resend.emails.send({
-      from: "BuildSetu <onboarding@resend.dev>",
+      from: "BuildSetu <noreply@buildsetuapp.com>",
       to: [to],   
       subject,
       html,
